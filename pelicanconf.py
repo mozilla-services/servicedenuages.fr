f42780e0517ba93a9d5d108ac2d740dd024a7d15
Alexis Métaireau <alexis@notmyidea.org>
2015-07-01 12:34:37 +0200

    Add a minisite for each supported language.
    Caveats:
    
    - URLs are not fully compatible with the current version (but I think that's
      okay for now since we don't have a lot of audience);
    - The site isn't translated, even if it opens up the possibility for it.

--- pelicanconf.py
+++ pelicanconf.py
@@ -42,6 +42,10 @@ MENUITEMS = (
 STATIC_PATHS = ['images', 'documents', 'extra/CNAME', 'presentations']
 EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'}}
 
+ARTICLE_URL = '{url}.html'
+ARTICLE_SAVE_AS = '{url}.html'
+
 PLUGIN_PATHS = ['plugins']
-PLUGINS = ['post_stats', 'better_figures_and_images']
+PLUGINS = ['post_stats', 'better_figures_and_images', 'i18n_subsites']
+I18N_SUBSITES = {'en': {}, }  # Override any default settings.
 RESPONSIVE_IMAGES = True
