24f7db85597cf2fcfd558c16c978a7d188cedac4
dan phrawzty <phrawzty+github@gmail.com>
2015-08-17 18:14:42 +0200

    kinto notifications preamble
    updates

--- pelicanconf.py
+++ pelicanconf.py
@@ -46,6 +46,7 @@ ARTICLE_URL = '{url}.html'
 ARTICLE_SAVE_AS = '{url}.html'
 
 PLUGIN_PATHS = ['plugins']
-PLUGINS = ['post_stats', 'better_figures_and_images', 'i18n_subsites']
+PLUGINS = ['post_stats', 'better_figures_and_images', 'i18n_subsites',
+           'liquid_tags.graphviz']
 I18N_SUBSITES = {'en': {}, }  # Override any default settings.
 RESPONSIVE_IMAGES = True
